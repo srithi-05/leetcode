2371270a77fc0791cf3e906e8f4c0c65cb34186f
srithi-05 <srithigampala5@gmail.com>
2026-03-26 20:20:55 +0530

    Add Two Sum solutions with brute force and HashMap
    Implemented two approaches for the Two Sum problem: a brute force method with O(n^2) time complexity and an optimized method using HashMap with O(n) time complexity.

--- Day-01-Two-Sum.java
+++ Day-01-Two-Sum.java
@@ -1,1 +1,43 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+//Approach 1: Brute Force 
+// Time: O(n^2)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int sum=nums[i]+nums[j];
+                if(sum==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+// Approach 2: Optimized (HashMap)
+// Time: O(n)
+import java.util.*;
+class Optimal {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
+
+
+//HashMap reduces time to O(n)
 
