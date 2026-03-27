d5e67e18fde6db85f64dec66db2e329739b46040
srithi-05 <srithigampala5@gmail.com>
2026-03-27 21:24:52 +0530

    Add palindrome number checker in Day-02-Palindrome-Num.java
    Implemented an optimized palindrome check for integers.

---
+++ Day-02-Palindrome-Num.java
@@ -0,0 +1,38 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+// Example 1:
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s=String.valueOf(x);
+        int l=0;
+        int r=s.length()-1;
+        while(l<=r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+        
+    }
+}
+//Optimal
+//time complexity: O(1)
+class Solution {
+    public boolean isPalindrome(int x) {
+        // negative or ending with 0 (but not 0 itself)
+        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int reversedHalf = 0;
+        while(x > reversedHalf){
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x = x / 10;
+        }
+        // even length → x == reversedHalf
+        // odd length → x == reversedHalf / 10
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+}
